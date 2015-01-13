465582aede965adccce6f795e9de15209db4420d
John Christian <john.r.christian@gmail.com>
2015-01-13 09:29:28 -0800

    Initial work on part A
    OpenCV header files (highgui.h) aren't being found in the system, so
    this code is not compiling.

--- set1/mecs132_1a.cpp
+++ set1/mecs132_1a.cpp
@@ -1,6 +1,14 @@
-#include "iostream"
+/*
+ * ME/CS 132A - Assigment 1, Part A
+ * Open and display an image using OpenCV
+ *
+ */
+
+#include <iostream>
+#include <highgui.h>
+#include <cv.h>
 
 int main(){
-    std::cout << "Hello, world!\n";
+    Mat imgdata = imread("First_Image.bmp", 1);
     return 0;
 }
